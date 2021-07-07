142f0b36599978f42a7cdd6044e650f64fd272e2
Mike Smithwick <mikes@distantsuns.com>
2021-07-06 19:23:35 -0700

    Two new files

---
+++ SwiftUIGallery/Items/Pickers/SwiftUIColorPicker .swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIColorPicker.swift
+//  SwiftUIGallery
+//
+//  Created by mike on 6/28/21.
+//
+
+import SwiftUI
+
+struct SwiftUIColorPicker: View {
+    var body: some View {
+        Text("Hello, colors!!")
+    }
+}
+   
+struct SwiftUIColorPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIColorPicker()
+    }
+}
--- SwiftUIGallery/Items/Pickers/SwiftUIMenuPicker.swift
+++ SwiftUIGallery/Items/Pickers/SwiftUIMenuPicker.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct SwiftUIMenuPicker: View {
-    var body: some View {
-        Text("Hello, MenuPicker")
-    }
+	let apolloPatches = ApolloPatches().apolloPatches
+	@State private var selectedFrameworkIndex = 0
+	
+	var body: some View {
+		NavigationView {
+			
+			//the InlinePicker is the default when adding to a form.
+			//PickerStyle can be used for other types. Interestingly enough
+			//specifying the InlinePickerStyle() forces the use of
+			//the Wheels. I think this a product of how a type of picker
+			//can be overriden by a system default. Doesn't make sense here.
+			
+			Form {
+				Section {
+					Picker(selection: $selectedFrameworkIndex, label: Text("Apollo Patches")) {
+						ForEach(0 ..< apolloPatches.count) {
+//							Text(self.apolloPatches[$0])
+							
+							let patchName = apolloPatches[$0]
+							let tempText = Text(patchName)
+							let tempImage = Image(patchName).resizable().frame(width:20,height:20,alignment:.leading)
+
+							HStack{
+								tempImage
+								tempText
+							}
+						}
+					}.pickerStyle(MenuPickerStyle())
+				}
+			}
+			.navigationBarTitle("Favorites").foregroundColor(.blue)
+		}
+	}
 }
 
 struct SwiftUIMenuPicker_Previews: PreviewProvider {
