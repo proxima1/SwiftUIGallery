7f3d09bb2e1e6f594ac0155d57ecaee34199a2bc
Mike Smithwick <mikes@distantsuns.com>
2021-09-07 15:35:53 -0700

    stuff

---
+++ SwiftUIGallery/Items/ActionSheets/SwiftUISliderSheet.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUISliderSheet.swift
+//  SwiftUIGallery
+//
+//  Created by mike on 9/3/21.
+//
+
+import SwiftUI
+
+struct SwiftUISliderSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUISliderSheet()
+    }
+}
---
+++ SwiftUIGallery/Items/SwiftUILinks.swift
@@ -0,0 +1,44 @@
+//
+//  SwiftUIImages.swift
+//  SwiftUIGallery
+//
+//  Created by mike on 10/23/20.
+//
+
+import SwiftUI
+
+struct SwiftUILinks: View {
+    var body: some View {
+        //iOS 15/XC 13 way, using markdown in the next field
+//        Text("[Stackoverflow](https://stackoverflow.com)")
+        NavigationView(){
+            VStack{
+                Spacer()
+                Text("URL")
+                HStack{
+                    Link("🔗  Distant Suns trailer",
+                         destination: URL(string: "https://www.youtube.com/watch?v=2zeN63hZaFQ")!).font(.system(size: 35.0,weight:.light))
+                }
+                
+            let destination = SwiftUISliders()
+                Divider().opacity(0.0)
+                Text("Navigation Link")
+                NavigationLink(
+                    destination: destination
+                )
+                {
+                    Text("show slider page").font(.system(size: 35.0,weight: .light))
+                }
+                Spacer()
+                Spacer()
+            }
+        }.navigationBarTitle("links", displayMode: .inline)
+
+    }
+}
+
+struct SwiftUILinks_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUILinks()
+    }
+}
