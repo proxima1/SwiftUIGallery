f279a926bc98ad72195014c34e2e2ec26c59aaad
Mike Smithwick <mikes@distantsuns.com>
2023-04-03 15:26:01 -0700

    Text issue

--- SwiftUIGallery/Items/SwiftUIText.swift
+++ SwiftUIGallery/Items/SwiftUIText.swift
@@ -166,6 +166,10 @@ struct SwiftUIText: View {
                 Section(header: Text("Multiline").modifier(SectionTitle())){
                     multiLineText()
                 }
+                
+                Section(header: Text("Attributed").modifier(SectionTitle())){
+                    attributedText()
+                }
             }
         }.padding(2.0).navigationBarTitle("Text", displayMode: .inline)
     }
@@ -206,12 +210,54 @@ struct multiLineText: View
     {
         VStack{
                                 Text("Where does it come from? Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old")
-//                                Text("Where does it come from? Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage")
                                     .font(.callout)
                                     .multilineTextAlignment(.center)
                                     .frame(width: 300)
                               
         }
+    }
+}
+
+struct attributedText: View{
+    var title: String = ""
+    
+//    var attributedString = AttributedString("The first month of your subscription is free.")
+//    let range = attributedString.range(of: "subscription")!
+//    attributedString[range].foregroundColor = .green
+    
+    var body: some  View
+    {
+        VStack{
+            buildSimpleAttributedStrings()
+            buildMarkupAttributedString()
+        }
+    }
+    
+    func buildSimpleAttributedStrings()->some View{
+        
+        var attributedString = AttributedString("The first month of your subscription is free.")
+        var range = attributedString.range(of: "free")!
+        attributedString[range].foregroundColor = .green
+        range = attributedString.range(of: "subscription")!
+        attributedString[range].foregroundColor = .blue
+        attributedString[range].underlineStyle = Text.LineStyle(pattern: .solid, color: .blue)
+
+        var text1=Text(attributedString)
+        
+        return text1
+    }
+    
+    func buildMarkupAttributedString()->some View{
+        
+        var attributedString = try! AttributedString(markdown: "Using **Markdown:** The first **month** of your subscription is _free._")
+        var range = attributedString.range(of: "free")!
+        attributedString[range].foregroundColor = .green
+        range = attributedString.range(of: "subscription")!
+        attributedString[range].foregroundColor = .blue
+        attributedString[range].underlineStyle = Text.LineStyle(pattern: .solid, color: .red)
+        var text1=Text(attributedString)
+        
+        return text1
     }
 }
 
